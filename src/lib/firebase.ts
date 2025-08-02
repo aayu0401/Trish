8026167e73d38c44c46b07ae27001cdcc1930c36
Aayu Shukla <aayushshkl0401@gmail.com>
2025-08-02 13:56:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,11 +8,13 @@ const firebaseConfig = {
   apiKey: "AIzaSyA_bP-0DxssNAEEqu-WKXzLKAUucTTr_4o",
   authDomain: "trish-j5ujv.firebaseapp.com",
   projectId: "trish-j5ujv",
-  storageBucket: "trish-j5ujv.firebasestorage.app",
+  storageBucket: "trish-j5ujv.appspot.com",
   messagingSenderId: "25505163931",
   appId: "1:25505163931:web:5b61e1ce34cc13117e343e"
 };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-const auth = getAuth(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
